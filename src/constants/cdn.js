bf65a99e4e327919f74145b265b805dd56cdf575
guangzan <guangzan1999@outlook.com>
2022-02-14 11:15:56 +0800

    auto deploy

--- src/constants/cdn.js
+++ src/constants/cdn.js
@@ -1,11 +1,12 @@
-export const chartJs = "https://cdn.jsdelivr.net/npm/chart.js";
+export const chartJs =
+    "https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/Chart.js/3.5.1/chart.min.js";
 export const mediaZoomJs =
-  "https://cdn.bootcdn.net/ajax/libs/medium-zoom/1.0.6/medium-zoom.min.js";
+    "https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/medium-zoom/1.0.6/medium-zoom.min.js";
 export const APlayerJs =
-  "https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/aplayer/1.10.1/APlayer.min.js";
+    "https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/aplayer/1.10.1/APlayer.min.js";
 export const APlayerCss =
-  "https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/aplayer/1.10.1/APlayer.min.css";
+    "https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/aplayer/1.10.1/APlayer.min.css";
 export const notationJs =
-  "https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/rough-notation/0.5.1/rough-notation.iife.min.js";
+    "https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/rough-notation/0.5.1/rough-notation.iife.min.js";
 export const typedJs =
-  "https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/typed.js/2.0.11/typed.min.js";
+    "https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/typed.js/2.0.11/typed.min.js";
