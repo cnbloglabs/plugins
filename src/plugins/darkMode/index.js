b2a9da90796524035e5a0ea0abe2790b98f17364
guangzan <guangzan1999@outlook.com>
2022-07-16 17:43:01 +0800

    feat(darkMode): toggle code block theme

--- src/plugins/darkMode/index.js
+++ src/plugins/darkMode/index.js
@@ -1,37 +1,9 @@
-import { useDarkModeOptions } from "@acnb/options";
-
-/**
- * 在暗色皮肤和亮色皮肤之间切换
- * @param { String }
- * @param { Boolean }
- */
-function changeMode(mode, hasTransition = true) {
-    if (mode === "dark") {
-        $("html").attr("theme", "dark");
-        // setBackground('dark')
-        localStorage.modeType = "dark";
-        if (hasTransition) {
-            $("body").addClass("light-to-dark");
-        }
-        setTimeout(() => {
-            $("body").removeClass("light-to-dark");
-        }, 1200);
-    } else {
-        $("html").removeAttr("theme");
-        // setBackground('light')
-        localStorage.modeType = "light";
-        if (hasTransition) {
-            $("body").addClass("dark-to-light");
-        }
-        setTimeout(() => {
-            $("body").removeClass("dark-to-light");
-        }, 1200);
-    }
-}
+import { useDarkModeOptions } from '@acnb/options'
+import { __DEV__ } from '../../constants/env'
 
 /**
  * 覆盖自定义背景色
- * @param {String} mode
+ * @param {string} mode
  */
 // function setBackground(mode) {
 //     const {
@@ -55,61 +27,90 @@ function changeMode(mode, hasTransition = true) {
 // }
 
 /**
+ * 切换代码块深色、浅色主题
+ * @param {string} mode 'dark' | 'light'
+ */
+function setCodeTheme(mode) {
+    if (__DEV__) return
+
+    mode === 'dark'
+        ? window.highlighter.setTheme(window.darkModeCodeHighlightTheme)
+        : window.highlighter.setTheme(window.codeHighlightTheme)
+}
+
+/**
+ * 在暗色皮肤和亮色皮肤之间切换
+ * @param {string} mode 'dark' | 'light'
+ * @param {boolean} withTransition
+ */
+function changeMode(mode, withTransition = true) {
+    setCodeTheme(mode)
+    $('html').attr('theme', mode)
+    localStorage.modeType = mode
+
+    const transitionClassName =
+        mode === 'dark' ? 'light-to-dark' : 'dark-to-light'
+
+    if (withTransition) {
+        $('body').addClass(transitionClassName)
+    }
+
+    setTimeout(() => $('body').removeClass(transitionClassName), 1200)
+}
+
+/**
  * 初始化
- * @param {*} String defaultMode
- * @param {*} Boolean autoDark
- * @param {*} Boolean autoLight
+ * @param {string} darkDefault
+ * @param {boolean} autoDark
+ * @param {boolean} autoLight
  */
 function init(darkDefault, autoDark, autoLight) {
-    const hour = new Date().getHours();
-    const isNight = hour > 19 || hour <= 5;
-    const storage = localStorage.modeType;
+    const hour = new Date().getHours()
+    const isNight = hour > 19 || hour <= 5
+    const storage = localStorage.modeType
 
     const followStorage = () => {
         if (storage) {
-            storage === "dark"
-                ? changeMode("dark", false)
-                : changeMode("light", false);
-            return;
+            changeMode(storage, false)
+            return
         }
 
         if (!storage) {
-            if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-                changeMode("dark", false);
-            } else {
-                changeMode("light", false);
-            }
+            window.matchMedia('(prefers-color-scheme: dark)').matches
+                ? changeMode('dark', false)
+                : changeMode('light', false)
         }
-    };
+    }
 
     if (!storage && darkDefault) {
-        changeMode("dark", false);
-        return;
+        changeMode('dark', false)
+        return
     }
 
     if (isNight) {
-        autoDark ? changeMode("dark", false) : followStorage();
+        autoDark ? changeMode('dark', false) : followStorage()
     }
 
     if (!isNight) {
-        autoLight ? changeMode("light", false) : followStorage();
+        autoLight ? changeMode('light', false) : followStorage()
     }
 }
 
 /**
  * 处理皮肤切换按钮点击事件
  */
-function click() {
-    $(document).on("click", ".mode-change", () => {
-        const isDark = $("html").attr("theme");
-        isDark ? changeMode("light") : changeMode("dark");
-    });
+function listenToggleButtonClick() {
+    $(document).on('click', '.mode-change', () => {
+        const isDark = $('html').attr('theme') === 'dark'
+        isDark ? changeMode('light') : changeMode('dark')
+    })
 }
 
 export const darkMode = (theme, devOptions) => {
     const { enable, darkDefault, autoDark, autoLight } =
-        useDarkModeOptions(devOptions);
-    if (!enable) return;
-    init(darkDefault, autoDark, autoLight);
-    click();
-};
+        useDarkModeOptions(devOptions)
+
+    if (!enable) return
+    init(darkDefault, autoDark, autoLight)
+    listenToggleButtonClick()
+}
