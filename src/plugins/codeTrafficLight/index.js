16cebf582fdb16c6988d10f75fcfd76ff563185a
laggage <38829279+laggage@users.noreply.github.com>
2023-05-02 14:53:05 +0800

    fix(codeTrafficLight): use padding-top instead of padding

--- src/plugins/codeTrafficLight/index.js
+++ src/plugins/codeTrafficLight/index.js
@@ -9,6 +9,6 @@ export function codeTrafficLight(theme, devOptions) {
     if (!isPostDetailsPage()) return
 
     insertStyle(
-        `pre[class*='language-'].highlighter-prismjs,pre code.hljs{position:relative;padding:2.5em 1em 1em}pre[class*='language-'].highlighter-prismjs::before,pre code.hljs::before{content:'';position:absolute;top:10px;left:12px;width:12px;height:12px;background:#fe5f59;border-radius:50%;box-shadow:20px 0 #febb2c,40px 0 #29c73f;z-index:2;}`
+        `pre[class*='language-'].highlighter-prismjs,pre code.hljs{position:relative;padding-top:2.5em}pre[class*='language-'].highlighter-prismjs::before,pre code.hljs::before{content:'';position:absolute;top:10px;left:12px;width:12px;height:12px;background:#fe5f59;border-radius:50%;box-shadow:20px 0 #febb2c,40px 0 #29c73f;z-index:2;}`
     )
 }
